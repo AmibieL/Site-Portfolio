34ef53c8f513342eaad8d89a3c9ce2adc53c7d36
AmibieL <mixtergamer.czs@gmail.com>
2025-01-18 21:40:20 -0300

    feat: social media links

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function scrollToSection(sectionId) {
   const section = document.getElementById(sectionId);
   section.scrollIntoView({ behavior: "smooth" });
 }
+
+function socialMediaLink(link) {
+  window.open(link, "_blank");
+}
